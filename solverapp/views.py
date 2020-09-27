90aab4da6c7b0e59a721be9214fa389972794407
USAMAWIZARD <usamawizard@gmail.com>
2020-09-27 12:13:08 +0530

    all functionalities done

--- solverapp/views.py
+++ solverapp/views.py
@@ -4,15 +4,25 @@ from solverapp.solver import *
 # Create your views here.
 def displaysudoku(request):
     return  render(request,'sudoku.html')
+ 
+def convertdftoarray(df):
+    arr=[]
+    for i in df:
+        for j in i:
+            arr.append(j)
+  
+    return arr
+
 
 def solvesudoku(request):
-    
     sudokobord= pd.DataFrame(index=[0,1,2,3,4,5,6,7,8], columns=[0,1,2,3,4,5,6,7,8])
     canplaceonbord= pd.DataFrame(index=[0,1,2,3,4,5,6,7,8], columns=[0,1,2,3,4,5,6,7,8])
     sudokobord=sudokobord.fillna(0)
     caninsert=[]
+    arr=convertdftoarray(sudokobord.to_numpy())
 
     filldefaultvalues(sudokobord,request.POST)
+
     while chkifzeroindataframe(sudokobord):
         for horizontal in range(0,9):
             for virtical in range(0,9):
@@ -30,6 +40,6 @@ def solvesudoku(request):
         print(canplaceonbord)
         canplaceonbord= pd.DataFrame(index=[0,1,2,3,4,5,6,7,8], columns=[0,1,2,3,4,5,6,7,8])
     print(sudokobord)
-    return redirect('/')
+    return render(request,'sudoku.html',{'sudo':arr})
 
 
